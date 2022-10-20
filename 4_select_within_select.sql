0a28fa87fd68aadfbc1faab779e5775d67609101
Samuel ODaniels <samodan195@gmail.com>
2022-10-20 03:59:45 +0100

    chore(4_select_within_select): solve 1-7

---
+++ 4_select_within_select.sql
@@ -0,0 +1,92 @@
+-- 1. List each country name where the population is larger than that of 'Russia'.
+SELECT name
+FROM world
+WHERE
+  population >
+  (SELECT population
+  FROM world
+  WHERE name LIKE 'russia');
+
+/* 2. Show the countries in Europe with a per capita GDP greater than
+      'United Kingdom'. */
+SELECT name
+FROM world
+WHERE
+  continent LIKE 'europe'
+  AND (gdp/population) >
+  (SELECT gdp/population
+   FROM world
+   WHERE name LIKE 'united kingdom');
+
+/* 3. List the name and continent of countries in the continents containing
+      either Argentina or Australia. Order by name of the country. */
+SELECT
+  name,
+  continent
+FROM world
+WHERE
+  continent IN
+  (SELECT continent
+   FROM world
+   WHERE name IN('argentina', 'australia'))
+ORDER BY name;
+
+/* 4. Which country has a population that is more than United Kingom but less
+      than Germany? Show the name and the population. */
+SELECT
+  name,
+  population
+FROM world
+WHERE
+  population >
+  (SELECT population
+   FROM world
+   WHERE name LIKE 'united kingdom')
+  AND population < 
+  (SELECT population
+   FROM world
+   WHERE name LIKE 'germany');
+
+/* 5. Show the name and the population of each country in Europe. Show the
+      population as a percentage of the population of Germany. */
+SELECT
+  name,
+  CONCAT(
+    ROUND(
+      (population/
+      (SELECT population
+      FROM world
+      WHERE name LIKE 'germany')) * 100
+    ), '%'
+  ) AS percentage
+FROM world
+WHERE continent LIKE 'europe';
+
+/* 6 Which countries have a GDP greater than every country in Europe? [Give the
+     name only.] */
+SELECT name
+FROM world
+WHERE
+  gdp > ALL
+  (SELECT gdp
+   FROM world
+   WHERE continent LIKE 'europe'
+   AND gdp > 0);
+
+/* 7. Find the largest country (by area) in each continent, show the continent,
+      the name and the area: */
+SELECT
+  continent,
+  name,
+  area
+FROM world AS w_x
+WHERE
+  area >= ALL
+  (SELECT area
+  FROM world AS w_y
+  WHERE w_x.continent = w_y.continent
+  AND area > 0)
+
+/* 8. List each continent and the name of the country that comes first
+      alphabetically. */
+
